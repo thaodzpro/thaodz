69598a6dfeb1ae026ab81be78ea7a03889d82e42
thaodzpro <5112002abcde4@gmail.com>
2023-05-02 22:35:17 +0700

    int commit

---
+++ day1/main.py
@@ -0,0 +1,19 @@
+"""age_me =float(input("vui long nhap tuoi cua me "))
+age_con = float(input("vui long nhap tuoi cua con: "))
+age = age_me - age_con - 1
+print("hieu so tuoi:",age)
+if age < 18:
+    print("thang bo di tu")
+else: 
+    print("thang bo di lam")
+
+x = int(input("vui long nhap gia tri cho x: "))
+if x > 0:
+    print("x la so duong")
+elif x==0 :
+      print("x bang 0")
+else:print("x la so am")
+
+"""
+
+print("😊")
